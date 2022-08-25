366917394dc03b3e119fd90d34dd0df5001aca98
beckireby15@gmail.com <beckireby15@gmail.com>
2022-08-25 15:20:49 +0300

    changed test using DOMContentLoaded

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document
+  .getElementById("text")
+  .addEventListener("DOMContentLoaded", displayText());
+console.log(text);
+
+function displayText() {
+  document.getElementById("text").innerHTML = "This is really cool!";
+}
